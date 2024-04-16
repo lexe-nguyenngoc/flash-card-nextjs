ddd819b267e311c65439b7b27458f50cbb8196e2
Lexe <ngocle.ng1001@gmail.com>
2024-04-16 23:32:38 +0700

    feat: implement focus

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,16 @@
 "use client";
 import classNames from "classnames/bind";
-import { ChangeEvent, FormEvent, useState } from "react";
+import {
+  ChangeEvent,
+  FC,
+  FormEvent,
+  ForwardRefRenderFunction,
+  ForwardedRef,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  useState
+} from "react";
 import { IoIosSend } from "react-icons/io";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
@@ -21,59 +31,79 @@ interface ICard {
   data: FlashCard;
   onCorrect(): void;
 }
-const Card = ({ className, data, onCorrect }: ICard) => {
-  const [inputValue, setInputValue] = useState("");
-  const [status, setStatus] = useState<TypeStatus>(TypeStatus.DEFAULT);
 
-  const disabled = status === TypeStatus.CORRECT;
+export interface ICardRef {
+  onFocus: () => void;
+}
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setStatus(TypeStatus.DEFAULT);
-    setInputValue(e.target.value);
-  };
+const Card = forwardRef<ICardRef, ICard>(
+  ({ className, data, onCorrect }, ref) => {
+    const [inputValue, setInputValue] = useState("");
+    const [status, setStatus] = useState<TypeStatus>(TypeStatus.DEFAULT);
+    const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const disabled = status === TypeStatus.CORRECT;
 
-    const match = data.means.some(
-      (mean) => inputValue.toLowerCase().trim() === mean.toLowerCase().trim()
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setStatus(TypeStatus.DEFAULT);
+      setInputValue(e.target.value);
+    };
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const match = data.means.some(
+        (mean) => inputValue.toLowerCase().trim() === mean.toLowerCase().trim()
+      );
+
+      if (match) {
+        setStatus(TypeStatus.CORRECT);
+        setTimeout(onCorrect, 1000);
+        return;
+      }
+
+      setStatus(TypeStatus.INCORRECT);
+    };
+
+    useImperativeHandle(ref, () => ({
+      onFocus() {
+        inputRef.current?.focus();
+      }
+    }));
+
+    return (
+      <Box className={cx("wrapper", className)}>
+        <>
+          <p className={cx("word-caption")}>Word</p>
+          <h3 className={cx("word")}>{data.word}</h3>
+
+          <form onSubmit={handleSubmit} className={cx("form")}>
+            <input
+              ref={inputRef}
+              type="text"
+              placeholder="Type here"
+              value={inputValue}
+              onChange={handleInputChange}
+              className={cx("form-control", status)}
+              disabled={disabled}
+            />
+            <button
+              type="submit"
+              className={cx("form-btn")}
+              disabled={disabled}
+            >
+              {status === TypeStatus.CORRECT ? (
+                <AiOutlineLoading3Quarters className={cx("animate")} />
+              ) : (
+                <IoIosSend />
+              )}
+            </button>
+          </form>
+        </>
+      </Box>
     );
+  }
+);
 
-    if (match) {
-      setStatus(TypeStatus.CORRECT);
-      setTimeout(onCorrect, 1000);
-      return;
-    }
-
-    setStatus(TypeStatus.INCORRECT);
-  };
-
-  return (
-    <Box className={cx("wrapper", className)}>
-      <>
-        <p className={cx("word-caption")}>Word</p>
-        <h3 className={cx("word")}>{data.word}</h3>
-
-        <form onSubmit={handleSubmit} className={cx("form")}>
-          <input
-            type="text"
-            placeholder="Type here"
-            value={inputValue}
-            onChange={handleInputChange}
-            className={cx("form-control", status)}
-            disabled={disabled}
-          />
-          <button type="submit" className={cx("form-btn")} disabled={disabled}>
-            {status === TypeStatus.CORRECT ? (
-              <AiOutlineLoading3Quarters className={cx("animate")} />
-            ) : (
-              <IoIosSend />
-            )}
-          </button>
-        </form>
-      </>
-    </Box>
-  );
-};
-
+Card.displayName = "Card";
 export default Card;
--- src/components/FlashCardList/index.tsx
+++ src/components/FlashCardList/index.tsx
@@ -1,11 +1,11 @@
 "use client";
 import classNames from "classnames/bind";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 
 import { FlashCard } from "@/types";
 
-import Card from "../Card";
+import Card, { ICardRef } from "../Card";
 
 import styles from "./FlashCardList.module.scss";
 
@@ -17,8 +17,8 @@ interface IFlashCardList {
 
 const FlashCardList = ({ flashCards }: IFlashCardList) => {
   const [index, setIndex] = useState(0);
-
   const router = useRouter();
+  const inputRef = useRef<(ICardRef | null)[]>([]);
 
   const handleAnswerCorrect = () => {
     if (index === flashCards.length - 1) {
@@ -29,12 +29,19 @@ const FlashCardList = ({ flashCards }: IFlashCardList) => {
     setIndex((prev) => prev + 1);
   };
 
+  useEffect(() => {
+    inputRef.current[index]?.onFocus();
+  }, [index]);
+
   return (
     <div className={cx("wrapper")}>
       {flashCards.map((card, i) => (
         <Card
           key={card.id}
-          className={cx({ inactive: index !== i })}
+          ref={(ref) => {
+            inputRef.current[i] = ref;
+          }}
+          className={cx("card", { active: index === i })}
           data={card}
           onCorrect={handleAnswerCorrect}
         />
