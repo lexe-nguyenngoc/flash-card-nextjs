29dadaa14def9a18186a4c4e16d336c38d4deca9
Lexe <ngocle.ng1001@gmail.com>
2024-04-16 22:30:50 +0700

    feat: change text for header and page title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <div className={cx("header")}>
       <Link href="/" className={cx("logo")}>
-        LearnLet
+        EasyVocab
       </Link>
     </div>
   );
